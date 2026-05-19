ac80501cf5bbc8934e5621244bb6de75b3c60ffb
Osatohawen <127215674+osatohawen1997@users.noreply.github.com>
2026-05-20 00:25:15 +0100

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -3,6 +3,11 @@ include "../database-connection/connect-db.php";
 
 session_start();
 
+if (isset($_SESSION['admin_email'])) {
+    header("Location: dashboard.php");
+    exit();
+}
+
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Pragma: no-cache");
 header("Expires: Sat, 01 Jan 2000 00:00:00 GMT");
